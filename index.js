12cdc4674944f33ade3070c188295bb1d794bf64
LeTu <leanhtu0369@gmail.com>
2021-07-03 16:32:03 +0700

    up lesson 3

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const port = 3000
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+const users = [
+  { 
+    id: 1,
+    name: 'Tôi'
+  },
+  { 
+    id: 2,
+    name: 'Đây'
+  },
+]
+
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/users', function (req, res) {
+  res.render('users/index', { users })
+})
+
+app.get('/users/search', function (req, res) {
+  const q = req.query.q
+  const newUsers = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+  console.log(q, newUsers);
+
+  res.render('users/index', { 
+    curent: q,
+    users: newUsers 
+  })
 })
 
 app.listen(port, () => {
